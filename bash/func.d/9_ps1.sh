db52e92bc4bad64eebbd566b9b1165ba00383c8e
Anthony Ford <ford.anthonyj@gmail.com>
2013-03-19 10:49:28 -0500

    Colorifying

--- bash/func.d/9_ps1.sh
+++ bash/func.d/9_ps1.sh
@@ -16,13 +16,16 @@ function embiggen () {
 # Virtualenv name if in venv. Based on
 # http://engineerwithoutacause.com/show-current-virtualenv-on-bash-prompt.html
 function get_venv() {
+    compensate=0
     if [[ $VIRTUAL_ENV != "" ]]
     then
-        venv="(${VIRTUAL_ENV##*/})"
+        compensate=11
+        venv="$IGreen(${VIRTUAL_ENV##*/})$Color_Off"
     else
         venv=""
     fi
     echo -e "$venv"
+    return $compensate
 }
 
 # Git branch and dirtiness. Stolen from
@@ -36,22 +39,44 @@ function git_stuff() {
     _branch=$(git symbolic-ref HEAD 2>/dev/null)
     _branch=${_branch#refs/heads/} # apparently faster than sed
     branch="" # need this to clear it when we leave a repo
+    compensate=0
     if [[ -n $_branch ]]; then
-       branch="[${_branch}${dirty}]"
+        if [ "x$dirty" == "x" ]; then
+            branch="$IGreen[${_branch}]"
+        else
+            branch="$IRed[${_branch}${dirty}]"
+        fi
+        branch="$branch$Color_Off"
+        compensate=11
     fi
     echo -e $branch
+    return $compensate
 }
 
 # Right aligned prompt section, based on 
 # http://superuser.com/a/517110/164671
 function prompt_right() {
-    echo -e "$(get_venv) $(git_stuff)"
+    # Right alignment compensation factor
+    compensate=0
+    venv=`get_venv`
+
+    # Use return status to check how many chars to compensate for
+    # during right alignment.
+    compensate=$((compensate+PIPESTATUS))
+    gs=`git_stuff`
+
+    # Use return status to check how many chars to compensate for
+    # during right alignment.
+    compensate=$((compensate+PIPESTATUS))
+    echo -e "$venv $gs"
+
+    # Return the compensation factor so `generate_prompt` can use it.
+    return $compensate
 }
 
 function prompt_left() {
     out="\u@\h:"
-    if [[ $(pwd|grep $SANDBOX_HOME/) && "X$SANDBOX_NAME" != "X" ]]
-    then
+    if [[ $(pwd|grep $SANDBOX_HOME/) && "X$SANDBOX_NAME" != "X" ]]; then
         out="$out$(pwd|sed "s#$SANDBOX#{$SANDBOX_NAME}#")"
     else
         out="$out\w"
@@ -64,9 +89,15 @@ function prompt_line_2() {
 }
 
 function generate_prompt() {
+    # Right alignment compensation factor
     COMPENSATE=0
+    pr=`prompt_right`
+
+    #Use return status from prompt_right to set the compensation factor
+    COMPENSATE=$((COMPENSATE+PIPESTATUS))
+
     PS1=$(printf "%*s\r%s\n%s" "$(($(tput cols) + ${COMPENSATE}))"\
-        "$(prompt_right)" "$(prompt_left)" "$(prompt_line_2)")
+        "$pr" "$(prompt_left)" "$(prompt_line_2)")
     
 }
 
