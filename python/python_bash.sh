fddc2378a3ae600d1304b29adc03c0b3006fb0a4
Anthony Ford <Anthony.Ford@twdb.texas.gov>
2016-05-12 08:46:12 -0500

    Added script to move window to next monitor

--- python/python_bash.sh
+++ python/python_bash.sh
@@ -6,3 +6,7 @@ export PIP_RESPECT_VIRTUALENV=true
 export WORKON_HOME=~/.virtualenvs
 export PIP_VIRTUALENV_BASE=$WORKON_HOME
 export VIRTUAL_ENV_DISABLE_PROMPT=1
+
+if [ ! -f ~/.python_history ]; then
+    touch ~/.python_history
+fi
